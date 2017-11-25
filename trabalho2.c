a5af361d4e4008b97f4c266468c760d44dafee6a
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2017-11-25 20:18:50 -0200

    Acessos OK, falta Cliques e valores reais, e relatorio

--- trabalho2.c
+++ trabalho2.c
@@ -12,9 +12,12 @@ int main(void)
             DPAcesso=1.0,                   //Desvio Padrão da Média de Acessos
             tempoClique=2.0,                //Média de Tempo cliques nos anuncios
             DpClique=1.0,                   //Desvio Padrão da Média de Tempo de Cliques 
+            //tempoGravarLog=0.0000012,       //Tempo para gravação de log (4 milisegundos)
+            tempoGravarLog=4.0,       //Tempo para gravação de log (4 milisegundos)
+            DPGravarLog=1.0,                //Desvio Padrão tempo de Log
             tempoSimulacao=3600.0;          //Tempo total de Simulação (1 Hora)
     //Quantidade Facilities
-    int     qtdLog = 1000,                  //Quantidade de items a serem salvos no log (numero de facilities)
+    int     qtdParaGravarLog = 1000,         //Quantidade de items a serem salvos no log (numero de facilities)
             max_jobs=1000000;                //Quantidade máxima de acessos
 
 
@@ -26,11 +29,14 @@ int main(void)
             event, 
             fAcesso,                        //Facility Acesso
             fClique,                        //Facility Cliques
-            qtdTotalAcessos=0,              //Número de pessoas que chegaram ao estabelecimento
-            qtdTotalCliques=0,              //Número de pessoas que chegaram ao estabelecimento
+            qtdFilaLogAcesso=0,             //Número de pessoas que chegaram ao estabelecimento
+            qtdFilaLogCliques=0,              //Número de pessoas que chegaram ao estabelecimento
             tamanhoMaximoFilaAcesso=0,      //Tamanho máximo da fila de acessos
             tamanhoMaximoFilaCliques=0,     //Tamanho máximo da fila de cliques
-            qtdProcessosGerados=0;          //Quatidade de Processos Gerados
+            qtdProcessosGerados=0,          //Quatidade de Processos Gerados
+            i,
+            j;
+
 
     float tx_cheg;
 
@@ -42,7 +48,7 @@ int main(void)
     
     schedule(1,0.0,acesso); 
 
-    while (time()<tempoSimulacao && acessos<=max_jobs)
+    while (time()<tempoSimulacao && qtdProcessosGerados<=max_jobs)
     {
         cause(&event, &acesso);
         switch(event)
@@ -50,62 +56,90 @@ int main(void)
             /*
                 Acesso a algum site
              */
-            case 1: 
-                schedule(2,0.0,acesso);
+            case 1:                 
+                qtdFilaLogAcesso++;
                 acessos++;
-                qtdTotalAcessos++;
                 qtdProcessosGerados++;
-                acesso++;
+
                 //Grava Log
-                if (inq(fAcesso) >= qtdLog)
+                if(qtdFilaLogAcesso == qtdParaGravarLog)
+                {
                     schedule(2,0.0,acesso);
 
+                    acesso++;
+                    
+                    //Limpa Fila
+                    qtdFilaLogAcesso=0;
+                }
+                
                 //Novas Chegadas
                 schedule(1,expntl(tempoAcessoSite),acesso);
-                schedule(3,expntl(tempoClique),clique);
+                schedule(4,expntl(tempoClique),clique);
 
                 break;
             /*
                 Grava log Acesso
              */
             case 2:
-                if (request(fAcesso,acesso,0) == 0)
-                    release(fAcesso,acesso);
-                else
-                {
-                    if (inq(fAcesso) > tamanhoMaximoFilaAcesso)
-                        tamanhoMaximoFilaAcesso = inq(fAcesso);
-                }
+                    if (request(fAcesso,acesso,0) == 0)
+                    {
+                        schedule(3,normal(tempoGravarLog,DPGravarLog),acesso);
+                    }
+                    else
+                    {
+                        if (inq(fAcesso) > tamanhoMaximoFilaAcesso)
+                            tamanhoMaximoFilaAcesso = inq(fAcesso);
+                    }
+                break;
+            /*
+                LIBERA LOG ACESSO
+             */
+            case 3:
+                release(fAcesso,acesso);
                 break;
             /*
                 Cliques
              */
-            case 3:
+            /*case 4:
+                qtdFilaLogCliques++;
                 cliques++;
-                qtdTotalCliques++;
-                clique++;
+
                 //Grava Log
-                if (inq(fClique) >= qtdLog)
-                    schedule(4,0.0,acesso);
-                break;
+                if(qtdFilaLogCliques == qtdParaGravarLog)
+                {
+                    schedule(5,0.0,clique);
+
+                    clique++;
+                    
+                    //Limpa Fila
+                    qtdFilaLogCliques=0;
+                }
+                break;*/
             /*
                 Grava Log Cliques
              */
-            case 4:
+            /*case 5:
                 if (request(fClique,clique,0) == 0)
-                    release(fClique,clique);
-                else
-                {
-                    if (inq(fClique) > tamanhoMaximoFilaCliques)
-                        tamanhoMaximoFilaCliques = inq(fClique);
-                }
-                break;
+                    {
+                        schedule(6,normal(tempoGravarLog,DPGravarLog),clique);
+                    }
+                    else
+                    {
+                        if (inq(fClique) > tamanhoMaximoFilaCliques)
+                            tamanhoMaximoFilaCliques = inq(fClique);
+                    }
+                break;*/
+            /*
+                LIBERA LOG CLIQUE
+             */
+            /*case 6:
+                release(fAcesso,acesso);
+                break;*/
         }
     }
 
-    printf("\nNumero de acessos que chegaram: %d",acessos);
-    printf("\nTamanho maximo fila acesso: %d acessos",tamanhoMaximoFilaAcesso);
-    printf("\nTamanho maximo fila cliques: %d cliques",tamanhoMaximoFilaCliques);
+    printf("\nQuantidade de acessos: %d acessos",acessos);
+    printf("\nQuantidade de cliques: %d cliques",cliques);
     printf("\n\nPressione qualquer tecla para gerar o relatório");
     getchar();
     
